91cdfd6383ce52acd492e262ddc646b67b6be5cb
Kei Ishikawa <ishikawa.kei@ies.jp>
2024-05-12 18:18:57 +0900

    正規表現修正

--- src/main/config/index.ts
+++ src/main/config/index.ts
@@ -89,32 +89,36 @@ const defaults: StoreType = {
         name: 'trace',
         fontColor: '#8abeb7',
         pattern: [
-          { reg: '\\b(TRACE|Trace)\\b|\\b(trace)\\b:', flag: 'i' },
-          { reg: '\\[(trace|trac|tra|tr|t)\\]', flag: 'i' }
+          { reg: '\\b(TRACE|[Tt]race)\\b|\\[(TRACE|[Tt]race)\\]|\\((TRACE|[Tt]race)\\)', flag: 'i' },
+          { reg: '\\b(VERBOSE|[Vv]erbose)\\b|\\[(VERBOSE|[Vv]erbose)\\]|\\((VERBOSE|[Vv]erbose)\\)', flag: 'i' }
         ]
       },
       {
         name: 'debug',
         fontColor: '#b294bb',
-        pattern: [
-          { reg: '\\b(DEBUG|Debug)\\b|\\b(debug)\\b:', flag: 'i' },
-          { reg: '\\[(debug|dbug|dbg|de|d)\\]', flag: 'i' }
-        ]
+        pattern: [{ reg: '\\b(DEBUG|[Dd]ebug)\\b|\\[(DEBUG|[Dd]ebug)\\]|\\((DEBUG|[Dd]ebug)\\)', flag: 'i' }]
       },
       {
         name: 'info',
         fontColor: '#b5bd68',
         pattern: [
-          { reg: '\\b(HINT|INFO|INFORMATION|Info|NOTICE|II)\\b|\\b(info|information)\\b:', flag: 'i' },
-          { reg: '\\[(information|info|inf|in|i)\\]', flag: 'i' }
+          { reg: '\\b(INFO|[Ii]nfo)\\b|\\[(INFO|[Ii]nfo)\\]|\\((INFO|[Ii]nfo)\\)', flag: 'i' },
+          {
+            reg: '\\b(INFORMATION|[Ii]nformation)\\b|\\[(INFORMATION|[Ii]nformation)\\]|\\((INFORMATION|[Ii]nformation)\\)',
+            flag: 'i'
+          },
+          { reg: '\\b(HINT|[Hh]int)\\b|\\[(HINT|[Hh]int)\\]|\\((HINT|[Hh]int)\\)', flag: 'i' },
+          { reg: '\\b(NOTICE|[Nn]otice)\\b|\\[(NOTICE|[Nn]otice)\\]|\\((NOTICE|[Nn]otice)\\)', flag: 'i' }
         ]
       },
       {
         name: 'warning',
         fontColor: '#efc573',
         pattern: [
-          { reg: '\\b(WARNING|WARN|Warn|WW)\\b|\\b(warning)\\b:', flag: 'i' },
-          { reg: '\\[(warning|warn|wrn|wn|w)\\]', flag: 'i' }
+          {
+            reg: '\\b(WARNING|[Ww]arning|WARN|[Ww]arn)\\b|\\[(WARNING|[Ww]arning|WARN|[Ww]arn)\\]|\\((WARNING|[Ww]arning|WARN|[Ww]arn)\\)',
+            flag: 'i'
+          }
         ]
       },
       {
@@ -122,8 +126,17 @@ const defaults: StoreType = {
         fontColor: '#cb6565',
         fontStyle: 'italic bold',
         pattern: [
-          { reg: '\\b(ALERT|CRITICAL|EMERGENCY|ERROR|FAILURE|FAIL|Fatal|FATAL|Error)\\b|\\b(error)\\b:', flag: 'i' },
-          { reg: '\\[(error|eror|err|er|e|fatal|fatl|ftl|fa|f)\\]', flag: 'i' }
+          { reg: '\\b(CRITICAL|[Cc]ritical)\\b|\\[(CRITICAL|[Cc]ritical)\\]|\\((CRITICAL|[Cc]ritical)\\)', flag: 'i' },
+          { reg: '\\b(CRIT|[Cc]rit)\\b|\\[(CRIT|[Cc]rit)\\]|\\((CRIT|[Cc]rit)\\)', flag: 'i' },
+          { reg: '\\b(ALERT|[Aa]lert)\\b|\\[(ALERT|[Aa]lert)\\]|\\((ALERT|[Aa]lert)\\)', flag: 'i' },
+          {
+            reg: '\\b(EMERGENCY|[Ee]mergency)\\b|\\[(EMERGENCY|[Ee]mergency)\\]|\\((EMERGENCY|[Ee]mergency)\\)',
+            flag: 'i'
+          },
+          { reg: '\\b(ERROR|[Ee]rror)\\b|\\[(ERROR|[Ee]rror)\\]|\\((ERROR|[Ee]rror)\\)', flag: 'i' },
+          { reg: '\\b(FAILURE|[Ff]ailure)\\b|\\[(FAILURE|[Ff]ailure)\\]|\\((FAILURE|[Ff]ailure)\\)', flag: 'i' },
+          { reg: '\\b(FAIL|[Ff]ail)\\b|\\[(FAIL|[Ff]ail)\\]|\\((FAIL|[Ff]ail)\\)', flag: 'i' },
+          { reg: '\\b(FATAL|[Ff]atal)\\b|\\[(FATAL|[Ff]atal)\\]|\\((FATAL|[Ff]atal)\\)', flag: 'i' }
         ]
       },
       {
@@ -132,7 +145,8 @@ const defaults: StoreType = {
         pattern: [
           { reg: '\\b\\d{4}-\\d{2}-\\d{2}(T|\\b)', flag: '' },
           { reg: '(?<=(^|\\s))\\d{4}[^ws]\\d{2}[^ws]\\d{2}\\b', flag: '' },
-          { reg: '\\d{1,2}:\\d{2}(:\\d{2}([.,]\\d{1,})?)?(Z| ?[+-]\\d{1,2}:\\d{2})?\\b', flag: '' }
+          { reg: '\\d{1,2}:\\d{2}(:\\d{2}([.,]\\d{1,})?)?(Z| ?[+-]\\d{1,2}:\\d{2})?\\b', flag: '' },
+          { reg: '[A-Z][a-z]{2}\\s+\\d{1,2}\\s+\\d{2}:\\d{2}:\\d{2}', flag: '' }
         ]
       },
       {
@@ -155,7 +169,7 @@ const defaults: StoreType = {
         name: 'constant',
         fontColor: '#4e86c4',
         pattern: [
-          { reg: '\\b(?<!\\w)(\\d+$|TRUE|[Tt]rue|FALSE|[Fa]alse|NULL|[Nn]ull)(?!\\w)\\b', flag: '' },
+          { reg: '\\b(?<!\\w)(\\d+$|TRUE|[Tt]rue|FALSE|[Ff]alse|NULL|[Nn]ull)(?!\\w)\\b', flag: '' },
           { reg: '\\b(0x[a-fA-F0-9]+)\\b', flag: '' }
         ]
       },
